6ecfc95609da10cca3bf35c038ee73851c17080b
Francesco <firefelix@gmail.com>
2016-09-22 11:49:02 +0200

    Initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { props, skinnable, t } from 'revenge';
+
+@skinnable()
+@props({
+  seconds: t.Number
+})
+export default class Counter extends React.Component {
+
+  getLocals() {
+    const seconds = this.props.seconds;
+    const minutes = Math.floor(seconds / 60);
+    return {
+      minutes,
+      seconds: seconds - (60 * minutes)
+    };
+  }
+
+  template({ minutes, seconds }) {
+    return (
+      <div>{minutes} minutes, {seconds} seconds elapsed</div>
+    );
+  }
+}
